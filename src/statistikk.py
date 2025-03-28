1efbace3327b34146fad0177076b45d0f5e1fcc5
Abel <Emilapa@stud.ntnu.no>
2025-03-28 14:18:31 +0100

    statistikk funksjonen gjør statistikk

--- src/statistikk.py
+++ src/statistikk.py
@@ -1,25 +1,53 @@
 def statistikk():
-    
-    def average(data):
-        return np.mean(data)
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-    def std_dev(data):
-        return np.std(data)
+    #leser data fra csv-fil
+    df = pd.read_csv(r'data\lokasjonsdata.csv')
 
-    def median(data):
-        return np.median(data)
+    print("Lagrede byer", df.columns.tolist())
+    by1 = input("1")
+    by2 = input("2")
 
-    std_dev_max = average(df) + std_dev(df)
-    std_dev_min = average(df) - std_dev(df)   
+    if by1 not in df.columns and by2 not in df.columns:
+        print("kys")
+        return
+    elif by2 not in df.columns and by1 in df.columns:
+        data1 = df[by1]
+        average1 = np.mean(data1)
+        std_dev1 = np.std(data1)
+        stdev_1_max = average1 + std_dev1
+        stdev_1_min = average1 - std_dev1
+        print(f"Gjennomsnitt for lokasjon 1: {average1:.3f}")
+        print(f"Standardavvik for lokasjon 1: {std_dev1:.3f}")
+    elif by1 in df.columns and by2 in df.columns:
+        data1 = df[by1]
+        average1 = np.mean(data1)
+        std_dev1 = np.std(data1)
+        stdev_1_max = average1 + std_dev1
+        stdev_1_min = average1 - std_dev1
+        print(f"Gjennomsnitt for lokasjon 1: {average1:.3f}")
+        print(f"Standardavvik for lokasjon 1: {std_dev1:.3f}")
 
-    print(f"Gjennomsnitt: {average(df[0]):.3f}")
-    print(f"Standardavvik: {std_dev(df[0]):.3f}")
-    print(f"Median: {median(df[0]):.3f}")
+        data2 = df[by2]
+        average2 = np.mean(data2)
+        std_dev2 = np.std(data2)
+        stdev_2_max = average2 + std_dev2
+        stdev_2_min = average2 - std_dev2
+        print(f"Gjennomsnitt for lokasjon 2: {average2:.3f}")
+        print(f"Standardavvik for lokasjon 2: {std_dev2:.3f}")
+        
 
-    plt.plot(range(0,len(data.values())),data.values())
-    plt.axhline(average(df[0]), color='red', linestyle='dashed', linewidth=1, label='Gjennomsnitt')
-    plt.axhline(median(df[0]), color='green', linestyle='dashed', linewidth=1, label='Median')
-    plt.axhline(std_dev_max, color='purple', linestyle='dashed', linewidth=1, label='Standardavvik')
-    plt.axhline(std_dev_min, color='purple', linestyle='dashed', linewidth=1)
+
+    plt.plot(data1)
+    plt.plot(data2)
+    plt.axhline(average1, color='red', linewidth=1, label='Gjennomsnitt')
+    plt.axhline(stdev_1_max, color='purple', linestyle='dashed', linewidth=1, label='Standardavvik')
+    plt.axhline(stdev_1_min, color='purple', linestyle='dashed', linewidth=1)
+    plt.axhline(average2, color='red', linewidth=1, label='Gjennomsnitt')
+    plt.axhline(stdev_2_max, color='purple', linestyle='dashed', linewidth=1, label='Standardavvik')
+    plt.axhline(stdev_2_min, color='purple', linestyle='dashed', linewidth=1)
+
     plt.legend()
     plt.show()
