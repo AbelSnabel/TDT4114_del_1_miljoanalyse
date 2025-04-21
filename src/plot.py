060dce52c83b0e2f40e9c852b8c79ea8576db108
Abel <Emilapa@stud.ntnu.no>
2025-04-21 14:22:22 +0200

    legger til gitignore og fikser plotting

--- src/plot.py
+++ src/plot.py
@@ -2,12 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_data():
-    df = pd.read_csv(r'data\lokasjonsdata.csv')
-    df.reset_index(drop=False, inplace=True)
+    df = pd.read_csv(r'data\lokasjonsdata.csv', index_col=0, parse_dates=True)
     
-    for by in list(df.columns):
-        plt.plot(df[by])
-
-    print(list(df.columns))
+    for column in df.columns:
+        plt.plot(df.index, df[column], label=column)
+    
+    plt.xlabel("datoer")
+    plt.ylabel("solstråling(kwh/m^2)")
+    plt.title("Graf over solstråling på lokasjonene")
     plt.legend()
+    plt.grid()
     plt.show()
