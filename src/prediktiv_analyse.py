f555b49d6b1fbc4141984f79a0512a5ef90da52e
Abel <Emilapa@stud.ntnu.no>
2025-04-22 14:27:07 +0200

    prediktiv analyse som funker dritbra

--- src/prediktiv_analyse.py
+++ src/prediktiv_analyse.py
@@ -1,45 +1,37 @@
 #beregne gjennomsnitt av hver dag, helst for 5 år. Bruke en metode for å lage forecast for neste år.
-def forecast(by):
-    import pandas as pd
-    from prophet import Prophet
-    from prophet.plot import plot_plotly, plot_components_plotly
-    import matplotlib.pyplot as plt
+import pandas as pd
+from prophet import Prophet
+from prophet.plot import plot_plotly, plot_components_plotly
+import matplotlib.pyplot as plt
 
-    # Load the data
-    df =  pd.read_csv(r'data/lokasjonsdata.csv')  # Les inn dataen
-    df = df.rename(columns={"dato": "ds", "verdi": "y"})  # Omdøp kolonner
+#laster inn data fra df, bare de to første kolonnene, altså dato og den første byen 
+df = pd.read_csv(r'data/lokasjonsdata.csv', usecols=[0, 2])  # Load both date and value columns
+df.columns = ['ds', 'y']  # Rename columns to match Prophet's requirements
 
-    # Print the first few rows of the dataframe
-    print(df.head())
+#skriver dato-kolonnen til rett format
+df['ds'] = pd.to_datetime(df['ds'])
 
-    # Initialize the Prophet model
-    m = Prophet()
-    
-    # Fit the model to the data
-    m.fit(df)
+#initialiserer prophet modellen, med årlig sesongvariasjon
+modell = Prophet(yearly_seasonality=True,weekly_seasonality=False,daily_seasonality=True)
 
-    # Create a dataframe for future predictions
-    future = m.make_future_dataframe(periods=365)
-    
-    # Make predictions
-    forecast = m.predict(future)
-    
-    # Plot the forecast
-    fig1 = m.plot(forecast)
-    fig2 = m.plot_components(forecast)
+#lag en modell til de historiske dataene
+modell.fit(df)
 
-    plot_plotly(m, forecast)
-    plot_components_plotly(m, forecast)
+antall_år = 2
 
-    print("Lagrede byer", df.columns.tolist())
-    byer = input("Skriv inn kommaseparerte byer: ")
-    for by in byer.split(","):
-        forecast(df[by])
+#lager en dataframe for prediksjonen
+predikasjon = modell.make_future_dataframe(freq='D', periods=365*antall_år)
 
-    plt.legend()
-    plt.show()
+#bruker modellen for å finne de forutsette verdiene
+forecast = modell.predict(predikasjon)
 
 
+#resten er plotting
+figur1 = modell.plot(forecast)
+figur2 = modell.plot_components(forecast)
 
+plot_plotly(modell, forecast)
+plot_components_plotly(modell, forecast)
 
-
+plt.legend()
+plt.show()
