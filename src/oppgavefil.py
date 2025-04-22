00a83091dfb6b7c6fae10ef284e69d0263a1dfa6
Abel <Emilapa@stud.ntnu.no>
2025-04-22 14:34:14 +0200

    gjør slik at man kan bruke prediktiv analyse-programmet fra hovedprogrammet

--- src/oppgavefil.py
+++ src/oppgavefil.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 from hent_data import hent_data
 from plot import plot_data
 from statistikk import statistikk
+from prediktiv_analyse import prediktiv_analyse
 
 while True:
     print("1: Hent data")
     print("2: Plot data")
     print("3: Statistikk")
-    print("4: Avslutt")
+    print("4: Prediktiv Analyse")
+    print("5: Avslutt")
     svar = input()
     if svar == "1":
         hent_data()
@@ -16,4 +18,6 @@ while True:
     elif svar == "3":
         statistikk()
     elif svar == "4":
+        prediktiv_analyse()
+    elif svar == "5":
         break
